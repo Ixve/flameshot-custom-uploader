1d2b073289345ad0cbbe6fd764e28e6ccfc5bd43
insert edgy and cool name <synthh@christiandior.anonaddy.com>
2022-03-12 18:30:57 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,6 +12,7 @@ fi
 #SOME HOSTS MAY USE "KEY: " INSTEAD OF "AUTHORIZATION: " IN THE HEADERS, MAKE SURE TO CHANGE IT IF REQUIRED
 image_url=$(curl -X POST -F "file=@"$temp_file -H "authorization: "$auth -v "$url" 2>/dev/null)
 echo $image_url > /tmp/upload.json
+#DURING MY TESTING IVE FOUND SOME HOSTS MAY USE A DIFFERENT "IMAGEURL" RESPONSE FIELD IN THE JSON, MAKE SURE TO KEEP AN EYE OUT FOR THAT!
 cat /tmp/upload.json | jq ".imageURL" | tr -d '"' | xclip -sel c
 notify-send "Image URL copied to clipboard" "$image_url" -a "Flameshot" -i $temp_file
 rm $temp_file
