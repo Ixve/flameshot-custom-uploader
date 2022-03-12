6937fb8a475eb75400802950392dca3ba7b0c212
insert edgy and cool name <synthh@christiandior.anonaddy.com>
2022-03-12 18:30:46 +0100

    Create altscript.sh

---
+++ altscript.sh
@@ -0,0 +1,16 @@
+auth="KEY GOES HERE"
+url="URL GOES HERE"
+temp_file="/tmp/screenshot.png"
+flameshot gui -r > $temp_file
+
+if [[ $(file --mime-type -b $temp_file) != "image/png" ]]; then
+    rm $temp_file
+  notify-send "Screenshot aborted" -a "Flameshot" && exit 1
+fi
+
+image_url=$(curl -X POST -F "file=@"$temp_file -H "key: "$auth -v "$url" 2>/dev/null)
+echo $image_url > /tmp/upload.json
+#DURING MY TESTING IVE FOUND SOME HOSTS MAY USE A DIFFERENT "IMAGEURL" RESPONSE FIELD IN THE JSON, MAKE SURE TO KEEP AN EYE OUT FOR THAT!
+cat /tmp/upload.json | jq ".imageUrl" | tr -d '"' | xclip -sel c
+notify-send "Image URL copied to clipboard" "$image_url" -a "Flameshot" -i $temp_file
+rm $temp_file
