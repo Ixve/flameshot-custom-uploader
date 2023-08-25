163667f0641c8cf8f5c8aed67424669ff1520527
Euro-pol <52547763+Euro-pol@users.noreply.github.com>
2023-08-26 00:16:55 +0200

    added nest.rip script
    ye

---
+++ nestrip.sh
@@ -0,0 +1,19 @@
+auth="UPLOAD_KEY"
+url="https://nest.rip/api/files/upload"
+
+temp_file="/tmp/screenshot.png"
+flameshot gui -r > $temp_file
+
+if [[ $(file --mime-type -b $temp_file) != "image/png" ]]; then
+	rm $temp_file
+  notify-send "Screenshot aborted" -a "Flameshot" && exit 1
+fi
+
+#SOME HOSTS MAY USE "KEY: " INSTEAD OF "AUTHORIZATION: " IN THE HEADERS, MAKE SURE TO CHANGE IT IF REQUIRED
+image_url=$(curl -X POST -F "file=@"$temp_file -H "authorization: "$auth -v "$url" 2>/dev/null)
+echo $image_url > /tmp/upload.json
+#DURING MY TESTING IVE FOUND SOME HOSTS MAY USE A DIFFERENT "IMAGEURL" RESPONSE FIELD IN THE JSON, MAKE SURE TO KEEP AN EYE OUT FOR THAT!
+cat /tmp/upload.json | jq -r ".fileURL" | xclip -sel c
+real_image_url=$(cat /tmp/upload.json | jq -r ".fileURL")
+notify-send "Image URL copied to clipboard" "$real_image_url" -a "Flameshot" -i $temp_file
+rm $temp_file
