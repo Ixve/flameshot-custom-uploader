138bdd2541fece31215a69e00e8745ec15fcba37
synth <synthh@christiandior.anonaddy.com>
2023-08-28 19:40:29 +0200

    E-Z updated
    Updated script taken from SKRRRTTs GitHub repository.

--- ezhost.sh
+++ ezhost.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-auth="# !! YOUR KEY GOES HERE !! #"
+auth="API KEY HERE."
 url="https://api.e-z.host/files"
 
 temp_file="/tmp/screenshot.png"
@@ -13,6 +13,31 @@ fi
 
 image_url=$(curl -X POST -F "file=@"$temp_file -H "key: "$auth -v "$url" 2>/dev/null)
 echo $image_url > /tmp/upload.json
+response_file="/tmp/upload.json"
+
+if ! jq -e . >/dev/null 2>&1 < /tmp/upload.json; then
+    notify-send "Error occurred while uploading. Please try again later." -a "Flameshot"
+    rm $temp_file
+    rm $response_file
+    exit 1
+fi
+
+success=$(cat /tmp/upload.json | jq -r ".success")
+if [[ "$success" != "true" ]] || [[ "$success" == "null" ]]; then
+    error=$(cat /tmp/upload.json | jq -r ".error")
+    if [[ "$error" == "null" ]]; then
+        notify-send "Error occurred while uploading. Please try again later." -a "Flameshot"
+        rm $temp_file
+        rm $response_file
+        exit 1
+    else
+        notify-send "Error: $error" -a "Flameshot"
+        rm $temp_file
+        rm $response_file
+        exit 1
+    fi
+fi
+
 cat /tmp/upload.json | jq -r ".imageUrl" | xclip -sel c
 notify-send "Image URL copied to clipboard" -a "Flameshot" -i $temp_file
 rm $temp_file
